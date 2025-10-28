f783c115b1bfa0296c3ae4632c713951e4b9fe59
“Tristian <tristian.davis@atlasschool.com>
2025-10-28 13:10:22 -0500

    setting up firestore and addpost

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +26,5 @@ const analytics = getAnalytics(app);
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
+export const storage = getStorage(app);
